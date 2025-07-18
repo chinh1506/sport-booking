17892e44ed317ba92d8e17c98bac73cc56537a36
chinh1506 <chinhhoang1506@gmail.com>
2025-07-18 08:18:44 +0700

    Update layout

---
+++ booking-webapp/src/app/booking/page.tsx
@@ -0,0 +1,12 @@
+import ComplexList from "@/components/ComplexList";
+import { LayoutWithNavbar } from "@/components/Layout/BookingCourtLayout";
+
+function BookingHomePage() {
+    return (
+        <LayoutWithNavbar>
+            <ComplexList></ComplexList>
+        </LayoutWithNavbar>
+    );
+}
+
+export default BookingHomePage;
--- booking-webapp/src/app/page.tsx
+++ booking-webapp/src/app/page.tsx
@@ -1,12 +1,10 @@
 import ComplexList from "@/components/ComplexList";
-import { LayoutWithNavbar } from "@/components/Layout";
-import LeafletMap from "@/components/LeafletMap";
+import { LayoutWithNavbar } from "@/components/Layout/BookingCourtLayout";
+// import LeafletMap from "@/components/LeafletMap";
 // import "leaflet/dist/leaflet.css"
 
 export default function Campaign() {
     return (
-        <LayoutWithNavbar>
-            <ComplexList></ComplexList>
-        </LayoutWithNavbar>
+        <div>Home page</div>
     );
 }
